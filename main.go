06380e78045020a355a5dd16f0946b73546d83d9
Lanthong <98867063+lxcs20@users.noreply.github.com>
2023-12-06 23:07:31 +0700

    control

--- main.go
+++ main.go
@@ -4,18 +4,22 @@ import (
 	"fmt"
 )
 
-
 func main() {
-	
+
 	fmt.Println("hello! Go")
 	fmt.Println("Go! Go!")
-	
+
 	// struct_()
 	// variable()
-	ifControl()
+	// ifControl()
+	// switchCase()
+	// shortIf()
+	// forLoop()
+	// doWhileLoop()
+	whileLoop()
 }
 
-func variable () {
+func variable() {
 	var fname string
 	var lname string
 	var age int
@@ -36,37 +40,85 @@ func variable () {
 }
 
 type Person struct {
-	fname string
-	lname string
+	fname    string
+	lname    string
 	birthday string
-	gender string
+	gender   string
 }
-func struct_ () {
+
+func struct_() {
 	person := Person{
-		fname: "lanthong",
-		lname: "myLast",
+		fname:    "lanthong",
+		lname:    "myLast",
 		birthday: "0X-0X-X00X",
-		gender: "M",
+		gender:   "M",
 	}
 
 	fmt.Println("hello, ", person)
 }
 
-func ifControl (){
+func ifControl() {
 	num := 10
-	if(num >= 10){
+	if num >= 10 {
 		fmt.Println(num)
 	} else {
 		fmt.Println("else case")
 	}
 
-	if(num < 10){
+	if num < 10 {
 		fmt.Println(num)
-	} else if (num == 5){
+	} else if num == 5 {
 		fmt.Println(num)
 	} else {
 		fmt.Println(num)
 	}
 }
 
+func switchCase() {
+	str := "b"
+	switch str {
+	case "a":
+		fmt.Println(str)
+	case "b":
+		fmt.Println(str)
+	case "bb":
+		fmt.Println(str)
+	default:
+		fmt.Println("not matching")
+	}
+}
+
+func shortIf(){
+	num1 := 5
+	num2 := 10
+
+	if sumNum := num1 + num2; sumNum >= 10 {
+		fmt.Println(sumNum)
+	} 
+}
+
+func forLoop(){
+	for i:=0; i <= 10; i++ {
+		fmt.Println(i)
+	}
+}
+
+func doWhileLoop (){
+	i:=1
+	for {
+		fmt.Printf("number: %d\n", i)
+		i++
+		if(i >= 10){
+			break
+		}
+	}	
+}
+
+func whileLoop (){
+	i:=0
+	for i <= 10 {
+		fmt.Println(i)
+		i++
+	}
+}
 
