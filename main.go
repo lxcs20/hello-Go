ba926b7e9e834da21225deb7acb3c167d1489e34
Lanthong <98867063+lxcs20@users.noreply.github.com>
2023-12-08 11:43:47 +0700

    08-12-2023

--- main.go
+++ main.go
@@ -9,14 +9,17 @@ func main() {
 	fmt.Println("hello! Go")
 	fmt.Println("Go! Go!")
 
-	// struct_()
 	// variable()
 	// ifControl()
 	// switchCase()
 	// shortIf()
 	// forLoop()
 	// doWhileLoop()
-	whileLoop()
+	// whileLoop()
+	// Array()
+	// slice()
+	// Map()
+	struct_()
 }
 
 func variable() {
@@ -37,24 +40,6 @@ func variable() {
 	fmt.Println(age)
 	fmt.Println(float)
 	fmt.Println(boolean)
-}
-
-type Person struct {
-	fname    string
-	lname    string
-	birthday string
-	gender   string
-}
-
-func struct_() {
-	person := Person{
-		fname:    "lanthong",
-		lname:    "myLast",
-		birthday: "0X-0X-X00X",
-		gender:   "M",
-	}
-
-	fmt.Println("hello, ", person)
 }
 
 func ifControl() {
@@ -88,37 +73,131 @@ func switchCase() {
 	}
 }
 
-func shortIf(){
+func shortIf() {
 	num1 := 5
 	num2 := 10
 
 	if sumNum := num1 + num2; sumNum >= 10 {
 		fmt.Println(sumNum)
-	} 
+	}
 }
 
-func forLoop(){
-	for i:=0; i <= 10; i++ {
+func forLoop() {
+	for i := 0; i <= 10; i++ {
 		fmt.Println(i)
 	}
 }
 
-func doWhileLoop (){
-	i:=1
+func doWhileLoop() {
+	i := 1
 	for {
 		fmt.Printf("number: %d\n", i)
 		i++
-		if(i >= 10){
+		if i >= 10 {
 			break
 		}
-	}	
+	}
 }
 
-func whileLoop (){
-	i:=0
+func whileLoop() {
+	i := 0
 	for i <= 10 {
 		fmt.Println(i)
 		i++
 	}
 }
 
+func Array() {
+	var Arr [3]int
+	Arr[0] = 1
+	Arr[1] = 2
+	Arr[2] = 3
+	fmt.Println(Arr)
+}
+
+func slice() {
+	var slice []int
+	slice = append(slice, 1)
+	slice = append(slice, 2, 3, 4)
+
+	fmt.Println(slice)
+	fmt.Println(len(slice))
+	fmt.Println(slice[0:1])
+	fmt.Println(slice)
+
+}
+
+func Map() {
+	myMap := make(map[string]int)
+
+	myMap["apple"] = 3
+	myMap["banana"] = 5
+	myMap["orage"] = 9
+
+	fmt.Println("Apple: ", myMap["apple"])
+	myMap["banana"] = 19
+
+	delete(myMap, "apple")
+	for key, value := range myMap {
+		fmt.Printf("%s -> %d\n", key, value)
+	}
+
+	val, ok := myMap["melon"]
+	if ok {
+		fmt.Println("melon is: ", val)
+	} else {
+		fmt.Println("melon not found in myMap")
+	}
+
+	valOr, isHave := myMap["orage"]
+	if isHave {
+		fmt.Println("orange is ", valOr)
+	} else {
+		fmt.Println("orage not found in myMap")
+	}
+}
+
+type Person struct {
+	fname    string
+	lname    string
+	birthday string
+	gender   string
+	address Address
+}
+
+type Address struct {
+	village  string
+	district string
+	province string
+}
+
+func struct_() {
+	myInfo := Person{
+		fname:    "lanthong",
+		lname:    "myLast",
+		birthday: "0X-0X-X00X",
+		gender:   "M",
+		address: Address{
+			village: "vill",
+			district: "dist",
+			province: "province",
+		},
+	}
+
+	fmt.Println("hello, ", myInfo)
+
+	var studentS [2]Person
+
+	studentS[0] = Person{
+		fname: "std1",
+		lname: "std2",
+		birthday: "0X-0X-20XX",
+		address: Address{
+			village: "std1 vill",
+			district: "std1 dist",
+			province: "std1 prov",
+		},
+	}
+
+	fmt.Println(studentS[0])
+}
